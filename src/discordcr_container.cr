e6f75de8b6044bbce3550052602c1789d4504dc1
Zac Nowicki <zachnowicki@gmail.com>
2018-07-31 14:50:57 +0000

    Additional documentation

--- src/discordcr_container.cr
+++ src/discordcr_container.cr
@@ -10,12 +10,35 @@ module Discord
     end
   end
 
+  # Represents a collection of event handlers bound to methods that can
+  # be registered onto a client instance. Containers are defined by
+  # `include Container`, and then using the `Discord::Handler` annotation
+  # to assign an event handler to that method. Any method that does not
+  # have this annotation is ignored, so that you can build private
+  # helper methods for your handler.
+  #
+  # ```
+  # module MyHandlers
+  #  include Discord::Container
+  #
+  #  @[Discord::Handler(payload: ::Discord::Message)
+  #  def ping(payload)
+  #    return unless payload.content == "!ping"
+  #    client.create_message(payload.channel_id, "pong!")
+  #  end
+  # end
+  # ```
+  #
+  # `client` will reference the `Client` instance the container was registered
+  # onto. It can also be replaced by any other class using the
+  # `@[Container::Options(client_class: MyClient)]` annotation. This is useful
+  # for replacing it for a mock client for use in specs.
   module Container
     annotation Options
     end
 
     EVENTS = {
-      ::Discord::Message => "on_message_create"
+      ::Discord::Message => "on_message_create",
     }
 
     macro included
@@ -26,6 +49,7 @@ module Discord
         getter! client : ::Discord::Client
       {% end %}
 
+      # Registers this containers handlers onto the given `client`
       def register_on(client)
         @client = client
         {% verbatim do %}
