e6b45f633f3dc330053a7cc87c12a2da9c42c085
Zac Nowicki <zachnowicki@gmail.com>
2018-09-17 15:38:29 +0000

    Fix config loading process

--- examples/main.cr
+++ examples/main.cr
@@ -6,13 +6,14 @@ client = Discord::Client.new token: ENV["TOKEN"]
 File.open("./config.json", "r") do |file|
   parser = JSON::PullParser.new(file)
   parser.read_object do |key|
+    matched = false
     Discord::Plugin.plugins.each do |plugin|
       if plugin.class.to_s.underscore == key
         plugin.configure(parser)
-      else
-        parser.skip
+        matched = true
       end
     end
+    parser.skip unless matched
   end
 end
 
